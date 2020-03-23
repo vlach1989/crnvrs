6ae6aabc71ac45ce8a31a7739450eb586a50cc02
pvlach <vlach1989@gmail.com>
2020-03-23 21:23:33 +0100

    Updates

---
+++ precache-manifest.b559716d05a6b278a637b2671896c4b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ad82e4a5aec8ae778e20f652792361b",
+    "url": "/crnvrs/index.html"
+  },
+  {
+    "revision": "119c697a996f71971c7c",
+    "url": "/crnvrs/static/css/2.fcbb9a24.chunk.css"
+  },
+  {
+    "revision": "a9328ac226adc1125ebe",
+    "url": "/crnvrs/static/css/main.04330cba.chunk.css"
+  },
+  {
+    "revision": "119c697a996f71971c7c",
+    "url": "/crnvrs/static/js/2.75e33263.chunk.js"
+  },
+  {
+    "revision": "58a99440f7b8745565fe43f3326db5f3",
+    "url": "/crnvrs/static/js/2.75e33263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9328ac226adc1125ebe",
+    "url": "/crnvrs/static/js/main.7e75a8df.chunk.js"
+  },
+  {
+    "revision": "c0c3392a06a17963d1c3",
+    "url": "/crnvrs/static/js/runtime-main.273524a3.js"
+  }
+]);
